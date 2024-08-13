70a81b0e38ce921b524737025f001ebd9a710e12
YanzeJeremy <yanzewan@usc.edu>
2024-08-12 17:44:36 -0700

    inital code for LLM_advisor w/o test/debug

---
+++ src/chiron_utils/bots/LLM_advisor_bot.py
@@ -0,0 +1,250 @@
+"""Bots that carry out random orders and make random order proposals."""
+
+from abc import ABC
+import random
+from typing import ClassVar, Dict, List, Sequence,Tuple
+
+from daidepp import AND, PRP, XDO
+
+from chiron_utils.bots.baseline_bot import BaselineBot
+from chiron_utils.parsing_utils import dipnet_to_daide_parsing
+from chiron_utils.utils import get_other_powers
+from fairdiplomacy.agents.bqre1p_agent import BQRE1PAgent as PyBQRE1PAgent
+from fairdiplomacy.agents.player import Player
+import heyhi
+import torch
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftConfig, PeftModel
+from torch.nn import DataParallel
+
+
+class LLMProposerBot(BaselineBot, ABC):
+    """Bot that carries out random orders and sends random order proposals to other bots.
+
+    Because of the similarity between the advisor and player versions of this bot,
+    both of their behaviors are abstracted into this single abstract base class.
+    """
+
+    is_first_messaging_round = False
+
+    def __init__(self, base_model_name: str, adapter_path: str, tokenizer_path: str, device: str = 'cpu'):
+        super().__init__()
+        self.base_model_name = base_model_name
+        self.adapter_path = adapter_path
+        self.tokenizer_path = tokenizer_path
+        self.device = device
+        self.tokenizer, self.model = self.load_model(base_model_name, adapter_path, tokenizer_path, device)
+
+    @staticmethod
+    def load_model(base_model_name: str, adapter_path: str, tokenizer_path: str, device: str = 'cpu') -> Tuple[AutoTokenizer, PeftModel]:
+        """Load the model and tokenizer.
+
+        Args:
+            base_model_name (str): The base model name.
+            adapter_path (str): The path to the adapter.
+            tokenizer_path (str): The path to the tokenizer.
+            device (str): The device to load the model onto.
+
+        Returns:
+            Tuple[AutoTokenizer, PeftModel]: The loaded tokenizer and model.
+        """
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        model = AutoModelForCausalLM.from_pretrained(base_model_name)
+        model = PeftModel.from_pretrained(model, adapter_path)
+        if torch.cuda.device_count() > 1:
+            model = DataParallel(model)
+        model.to(device)
+        
+        return tokenizer, model
+
+    def generate_text(self, prompt: str) -> str:
+        """Generate text based on a given prompt.
+
+        Args:
+            prompt (str): The prompt to generate text from.
+
+        Returns:
+            str: The generated text.
+        """
+        input_ids = self.tokenizer.encode(prompt, return_tensors='pt').to(self.device)
+        
+        self.model.eval()
+        with torch.no_grad():
+            if isinstance(self.model, DataParallel):
+                output_ids = self.model.module.generate(input_ids, max_new_tokens=1024)
+            else:
+                output_ids = self.model.generate(input_ids, max_new_tokens=1024)
+
+        generated_text = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+        return generated_text
+    
+    def load_cicero(self):
+        """Load Cicero agent
+        """
+        agent_config = heyhi.load_config('/diplomacy_cicero/conf/common/agents/cicero.prototxt')
+        self.agent = PyBQRE1PAgent(agent_config.bqre1p)
+        return self.agent
+    
+    def format_prompt(self,agent,own,oppo) -> str:
+        MAPPING = {'AUS': 'AUSTRIA', 'ENG': 'ENGLAND', 'FRA': 'FRANCE', 'ITA': 'ITALY', 'RUS': 'RUSSIA', 'GER': 'GERMANY',
+           'TUR': 'TURKEY'}
+        if own in MAPPING.keys():
+            own = MAPPING[own]
+        if oppo in MAPPING.keys():
+            oppo = MAPPING[oppo]
+        SYS_PROMPT = """You are an AI assistant tasked with understanding and analyzing the board status of a Diplomacy game, the message history between two players, and the recommended orders by Cicero for the current phase.
+Your goal is to provide feedback on whether to trust the last message, based on the context provided.
+"""     
+        # system prompt format
+        system_prompt = f'<<SYS>>\n{SYS_PROMPT}\n<</SYS>>' if SYS_PROMPT is not None else ''
+        # board states format
+        board_states = self.game.get_state()['units']
+        for key, value in board_states.items():
+            board_states[key] = [string.replace('STP/NC', 'STP').replace('STP/SC','STP').replace('SPA/SC','SPA').replace('SPA/NC','SPA').replace('BUL/EC','BUL').replace('BUL/SC','BUL') for string in value]
+        sorted_board_states = {key: sorted(value) for key, value in board_states.items()}
+
+        # message format
+        messages = self.game.messages
+        filtered_messages = [
+            message for message in messages.values()
+            if (message.sender == own and message.recipient == oppo) or 
+            (message.sender == oppo and message.recipient == own)
+        ]
+        sorted_messages = sorted(filtered_messages, key=lambda x: x.time_sent, reverse=True)
+        closest_8_messages = sorted_messages[:8]
+        reversed_closest_8_messages = list(reversed(closest_8_messages))
+        my_message = ''
+        i = 0
+        while i < len(reversed_closest_8_messages):
+            my_message += f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' "
+            if i == len(reversed_closest_8_messages) - 1 and reversed_closest_8_messages[i].sender != oppo:
+                my_message = my_message.rsplit(f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' ", 1)[0]
+                reversed_closest_8_messages.pop()
+                i -= 1
+            i += 1
+
+        #cicero recommendation format
+        sender_player = Player(agent, own)
+        sender_orders = sender_player.get_orders(self.game)
+
+        prompt = f'<s>[INST] {system_prompt}\n    \n---\n\nBoard Status: {sorted_board_states}\n\nCicero Recommendation for {own}: {sender_orders}\n\nMessage History: {my_message}\n    \n---\n\nQuestion:As the advisor of {own}, Should we trust last message from {oppo}? [/INST]'
+        return prompt
+
+    async def start_phase(self) -> None:
+        """Execute actions at the start of the phase."""
+        self.is_first_messaging_round = True
+    
+
+    def get_random_proposal_orders(self) -> Dict[str, str]:
+        """Generate random order proposals for other powers.
+
+        Returns:
+            Mapping from powers to random order proposals.
+        """
+        # Getting the list of possible orders for all locations
+        possible_orders = self.game.get_all_possible_orders()
+
+        proposals = {}
+
+        # For each power, randomly sample a valid order
+        for other_power in get_other_powers([self.power_name], self.game):
+            suggested_random_orders = [
+                random.choice(possible_orders[loc])
+                for loc in self.game.get_orderable_locations(other_power)
+                if possible_orders[loc]
+            ]
+            suggested_random_orders = list(
+                filter(
+                    lambda x: x != "WAIVE" and not x.endswith("VIA"),
+                    suggested_random_orders,
+                )
+            )
+            if len(suggested_random_orders) > 0:
+                commands = dipnet_to_daide_parsing(suggested_random_orders, self.game)
+                random_orders = [XDO(command) for command in commands]
+                if len(random_orders) > 1:
+                    suggested_random_orders = PRP(AND(*random_orders))
+                else:
+                    suggested_random_orders = PRP(*random_orders)
+
+                proposals[other_power] = str(suggested_random_orders)
+
+        return proposals
+
+    async def do_messaging_round(self, agent,orders: Sequence[str]) -> List[str]:
+        """Carry out one round of messaging, along with related tasks.
+
+        Returns:
+            List of orders to carry out.
+        """
+        if not self.is_first_messaging_round:
+            return list(orders)
+
+        random_order_proposals = self.get_random_proposal_orders()
+
+        for other_power, suggested_random_orders in random_order_proposals.items():
+            if self.bot_type == "advisor":
+                prompt = self.format_prompt(agent,self.power_name,other_power)
+                generate_text = self.generate_text(prompt)
+                await self.suggest_message(other_power, (generate_text))
+                await self.suggest_message(other_power, (suggested_random_orders))
+            elif self.bot_type == "player":
+                await self.send_message(other_power, (suggested_random_orders))
+
+        self.is_first_messaging_round = False
+
+        return list(orders)
+
+    def get_random_orders(self) -> List[str]:
+        """Generate random orders to carry out.
+
+        Returns:
+            List of random orders.
+        """
+        possible_orders = self.game.get_all_possible_orders()
+        orders = [
+            random.choice(list(possible_orders[loc]))
+            for loc in self.game.get_orderable_locations(self.power_name)
+            if possible_orders[loc]
+        ]
+        return orders
+
+    async def gen_orders(self) -> List[str]:
+        """Generate orders for a turn.
+
+        Returns:
+            List of orders to carry out.
+        """
+        orders = self.get_random_orders()
+        if self.bot_type == "advisor":
+            await self.suggest_orders(orders)
+        return orders
+
+
+class RandomProposerAdvisor(LLMProposerBot):
+    """Advisor form of `RandomProposerBot`."""
+
+    bot_type: ClassVar[str] = "advisor"
+
+
+class RandomProposerPlayer(LLMProposerBot):
+    """Player form of `RandomProposerBot`."""
+
+    bot_type: ClassVar[str] = "player"
+
+if __name__ == "__main__":
+    bot = LLMProposerBot(
+        base_model_name="meta-llama/Llama-2-7b-chat-hf",
+        adapter_path="../models/finetuned_llama2_after_CPO/checkpoint-best-recent",
+        tokenizer_path="../models/finetuned_llama2_after_CPO/checkpoint-best-recent",
+        device="cuda"
+    )
+
+    cicero_agent = bot.load_cicero()
+    # Generate text from a prompt
+    # prompt = "Generate a random suggestion for the game"
+    # generated_text = bot.generate_text(cicero_agent,prompt)
+    orders = bot.get_random_orders()
+    bot.do_messaging_round(cicero_agent,orders)
+    # print(generated_text)
