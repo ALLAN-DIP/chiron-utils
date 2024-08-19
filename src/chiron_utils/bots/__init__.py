083a17dbbaff1c826d82b9f51ae305e3a1a2bf5d
YanzeJeremy <yanzewan@usc.edu>
2024-08-19 19:40:14 -0400

    Add `LlmAdvisor` using custom Llama 2 model
    Co-authored-by: Alex Hedges <git@alexhedges.dev>

--- src/chiron_utils/bots/__init__.py
+++ src/chiron_utils/bots/__init__.py
@@ -1,12 +1,14 @@
 """Module for CHIRON advisors and players."""
 
 from chiron_utils.bots.baseline_bot import BaselineBot as BaselineBot
+from chiron_utils.bots.llm_advisor_bot import LlmAdvisor as LlmAdvisor
 from chiron_utils.bots.random_proposer_bot import (
     RandomProposerAdvisor as RandomProposerAdvisor,
     RandomProposerPlayer as RandomProposerPlayer,
 )
 
 BOTS = [
+    LlmAdvisor,
     RandomProposerAdvisor,
     RandomProposerPlayer,
 ]
