5c1e91cfe154c18125cdf4ee87988c5a6c9e1262
Alexander Jephtha <ajep3697@uni.sydney.edu.au>
2024-08-21 11:17:01 +1000

    adding knn_bot to run_bot

--- src/chiron_utils/bots/__init__.py
+++ src/chiron_utils/bots/__init__.py
@@ -1,12 +1,14 @@
 """Module for CHIRON advisors and players."""
 
 from chiron_utils.bots.baseline_bot import BaselineBot as BaselineBot
+from chiron_utils.bots.knn_bot import KnnBot
 from chiron_utils.bots.random_proposer_bot import (
     RandomProposerAdvisor as RandomProposerAdvisor,
     RandomProposerPlayer as RandomProposerPlayer,
 )
 
 BOTS = [
+    KnnBot,
     RandomProposerAdvisor,
     RandomProposerPlayer,
 ]
