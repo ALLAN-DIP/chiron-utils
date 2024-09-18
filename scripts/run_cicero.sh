f843b1a9061583430871015c592494962e2ea282
Alex Hedges <git@alexhedges.dev>
2024-09-18 18:50:57 -0400

    Do not mount `diplomacy_cicero` repo in its container
    These files are contained within the OCI image, so it is redundant to
    mount them.

--- scripts/run_cicero.sh
+++ scripts/run_cicero.sh
@@ -7,7 +7,6 @@ HOST=$2
 POWER=$3
 shift 3
 
-REPO=$(realpath ~/diplomacy_cicero/)
 CICERO=/media/volume/cicero-base-models
 
 GAME_COMMAND=(
@@ -22,9 +21,6 @@ GAME_COMMAND=(
 time docker run \
   --rm \
   --gpus all \
-  --volume "$REPO"/fairdiplomacy/AMR/:/diplomacy_cicero/fairdiplomacy/AMR/:ro \
-  --volume "$REPO"/fairdiplomacy_external:/diplomacy_cicero/fairdiplomacy_external:ro \
-  --volume "$REPO"/parlai_diplomacy:/diplomacy_cicero/parlai_diplomacy:ro \
   --volume "$CICERO"/agents:/diplomacy_cicero/conf/common/agents:ro \
   --volume "$CICERO"/models:/diplomacy_cicero/models:ro \
   --volume "$CICERO"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2:ro \
