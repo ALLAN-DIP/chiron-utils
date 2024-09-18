c25994aa9059e7dbfe4084e03ed0b30b3d3aa898
Alex Hedges <git@alexhedges.dev>
2024-09-18 19:08:31 -0400

    Rename `CICERO` to `CICERO_DIR`
    This makes it clear that the variable is a directory.

--- scripts/run_cicero.sh
+++ scripts/run_cicero.sh
@@ -7,7 +7,7 @@ HOST=$2
 POWER=$3
 shift 3
 
-CICERO=/media/volume/cicero-base-models
+CICERO_DIR=/media/volume/cicero-base-models
 
 GAME_COMMAND=(
   python fairdiplomacy_external/mila_api.py
@@ -21,9 +21,9 @@ GAME_COMMAND=(
 time docker run \
   --rm \
   --gpus all \
-  --volume "$CICERO"/agents:/diplomacy_cicero/conf/common/agents:ro \
-  --volume "$CICERO"/models:/diplomacy_cicero/models:ro \
-  --volume "$CICERO"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2:ro \
+  --volume "$CICERO_DIR"/agents:/diplomacy_cicero/conf/common/agents:ro \
+  --volume "$CICERO_DIR"/models:/diplomacy_cicero/models:ro \
+  --volume "$CICERO_DIR"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2:ro \
   --workdir /diplomacy_cicero \
   ghcr.io/allan-dip/diplomacy_cicero:human_experiments-alex \
   "${GAME_COMMAND[@]}" "$@"
