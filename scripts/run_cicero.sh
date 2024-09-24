21d74f4354fe0608ae24c36449aa698e630a7ade
Alex Hedges <git@alexhedges.dev>
2024-09-24 14:02:40 -0400

    Update `run_cicero.sh` to run on Jetstream2
    I was able to slightly simplify the logic by using Docker instead of
    Singularity. I also needed to make some changes due to the differences
    between the two environments.

--- scripts/run_cicero.sh
+++ scripts/run_cicero.sh
@@ -5,15 +5,12 @@ set -euxo pipefail
 GAME_ID=$1
 HOST=$2
 POWER=$3
-CUDA_NUM=$4
-shift 4
+shift 3
 
-export WORK=$PWD
-REPO=$(realpath diplomacy_cicero/)
-export REPO
-CICERO=$(realpath cicero/)
-export CICERO
-export GAME_COMMAND=(
+REPO=$(realpath ~/diplomacy_cicero/)
+CICERO=/media/volume/cicero-base-models
+
+GAME_COMMAND=(
   python fairdiplomacy_external/mila_api.py
   --game_id "$GAME_ID"
   --host "$HOST"
@@ -21,17 +18,15 @@ export GAME_COMMAND=(
   --game_type 2
 )
 
-# Sets `CUDA_VISIBLE_DEVICES` inside the container
-# From https://docs.sylabs.io/guides/3.11/user-guide/gpu.html#multiple-gpus
-export SINGULARITYENV_CUDA_VISIBLE_DEVICES=$CUDA_NUM
-
-time singularity run --compat --nv \
-  --env OPENBLAS_NUM_THREADS=4 \
-  --bind "$REPO"/fairdiplomacy/AMR/:/diplomacy_cicero/fairdiplomacy/AMR/ \
-  --bind "$REPO"/fairdiplomacy_external:/diplomacy_cicero/fairdiplomacy_external \
-  --bind "$REPO"/parlai_diplomacy:/diplomacy_cicero/parlai_diplomacy \
-  --bind "$CICERO"/agents:/diplomacy_cicero/conf/common/agents \
-  --bind "$CICERO"/models:/diplomacy_cicero/models \
-  --bind "$CICERO"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2 \
-  --pwd /diplomacy_cicero \
-  cicero_latest.sif "${GAME_COMMAND[@]}" "$@"
+time docker run \
+  --rm \
+  --gpus all \
+  --volume "$REPO"/fairdiplomacy/AMR/:/diplomacy_cicero/fairdiplomacy/AMR/:ro \
+  --volume "$REPO"/fairdiplomacy_external:/diplomacy_cicero/fairdiplomacy_external:ro \
+  --volume "$REPO"/parlai_diplomacy:/diplomacy_cicero/parlai_diplomacy:ro \
+  --volume "$CICERO"/agents:/diplomacy_cicero/conf/common/agents:ro \
+  --volume "$CICERO"/models:/diplomacy_cicero/models:ro \
+  --volume "$CICERO"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2:ro \
+  --workdir /diplomacy_cicero \
+  ghcr.io/allan-dip/diplomacy_cicero:human_experiments-alex \
+  "${GAME_COMMAND[@]}" "$@"
