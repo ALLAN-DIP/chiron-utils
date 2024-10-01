ec0e2b6ead7e0b222e99c4b89b1e9108be19955d
YanzeJeremy <yanzewan@usc.edu>
2024-10-01 19:56:29 +0000

    update for llm_advisor_bot

--- src/chiron_utils/bots/llm_advisor_bot.py
+++ src/chiron_utils/bots/llm_advisor_bot.py
@@ -7,19 +7,18 @@ import sys
 from peft import PeftModel
 import torch
 from torch.nn import DataParallel
-from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
-
+from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer,AutoModelForSequenceClassification
 from chiron_utils.bots.baseline_bot import BaselineBot
 from chiron_utils.utils import POWER_NAMES_DICT, get_other_powers
+import numpy as np
+# try:
+#     from fairdiplomacy.agents.bqre1p_agent import BQRE1PAgent as PyBQRE1PAgent
+#     from fairdiplomacy.agents.player import Player
+#     import heyhi
 
-try:
-    from fairdiplomacy.agents.bqre1p_agent import BQRE1PAgent as PyBQRE1PAgent
-    from fairdiplomacy.agents.player import Player
-    import heyhi
-
-    USE_CICERO = True
-except ImportError:
-    USE_CICERO = False
+#     USE_CICERO = True
+# except ImportError:
+#     USE_CICERO = False
 
 
 @dataclass
@@ -33,15 +32,22 @@ class LlmAdvisor(BaselineBot):
     base_model_name = "meta-llama/Llama-2-7b-chat-hf"
     adapter_path: str = "usc-isi/Llama2-Advisor"
     tokenizer_path: str = "usc-isi/Llama2-Advisor"
+    classification_model_name: str = "AutonLabTruth/llama3_m"  # Added for classification model
+    classification_tokenizer_name: str = "meta-llama/Llama-3.1-8B"  # Added for classification tokenizer
     device: str = "cuda"
 
     def __post_init__(self):
         """Initialize models."""
+        # Load the main model and tokenizer
         self.tokenizer, self.model = self.load_model(
             self.base_model_name, self.adapter_path, self.tokenizer_path, self.device
         )
-        if USE_CICERO:
-            self.agent = self.load_cicero()
+
+        # Load the classification model and tokenizer
+        self.classification_model, self.classification_tokenizer = self.load_classification_model(
+            self.classification_model_name, self.classification_tokenizer_name, self.device
+        )
+
 
     @staticmethod
     def load_model(
@@ -59,20 +65,29 @@ class LlmAdvisor(BaselineBot):
             The loaded tokenizer and model.
         """
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        model = AutoModelForCausalLM.from_pretrained(base_model_name)
+        model = AutoModelForCausalLM.from_pretrained(base_model_name,torch_dtype=torch.float16)
         model = PeftModel.from_pretrained(model, adapter_path)
         if torch.cuda.device_count() > 1:
             model = DataParallel(model)
         model.to(device)
 
         return tokenizer, model
+    
+    def load_classification_model(
+        self, model_name: str, tokenizer_name: str, device: str
+    ) -> Tuple[AutoModelForSequenceClassification, AutoTokenizer]:
+        """Load the classification model and tokenizer."""
+        classification_model = AutoModelForSequenceClassification.from_pretrained(model_name,torch_dtype=torch.float16)
+        classification_tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        classification_model.to(device)
+        return classification_model, classification_tokenizer
 
 
-    @staticmethod
-    def load_cicero() -> "PyBQRE1PAgent":
-        """Load Cicero agent."""
-        agent_config = heyhi.load_config("/diplomacy_cicero/conf/common/agents/cicero.prototxt")
-        return PyBQRE1PAgent(agent_config.bqre1p)
+    # @staticmethod
+    # def load_cicero() -> "PyBQRE1PAgent":
+    #     """Load Cicero agent."""
+    #     agent_config = heyhi.load_config("/diplomacy_cicero/conf/common/agents/cicero.prototxt")
+    #     return PyBQRE1PAgent(agent_config.bqre1p)
 
     def generate_text(self, prompt: str) -> str:
         """Generate text based on a given prompt.
@@ -94,14 +109,32 @@ class LlmAdvisor(BaselineBot):
 
         generated_text = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
         return generated_text  # type: ignore[no-any-return]
+    
+    def classification(self, prompt: str) -> str:
+        """Classify a prompt to decide whether to trust or not trust."""
+        inputs = self.classification_tokenizer(prompt, return_tensors="pt").to(self.device)
+        self.classification_model.eval()
+        with torch.no_grad():
+            logits = self.classification_model(**inputs).logits
 
-    def get_cicero_order_recommendations(self, own: str) -> List[str]:
-        """Retrieve orders from CICERO."""
-        sender_player = Player(self.agent, own)
-        sender_orders = sender_player.get_orders(self.game)
-        return sender_orders  # type: ignore[no-any-return]
+        logits = logits.detach().cpu().numpy()
+        softmaxed = np.exp(logits) / np.sum(np.exp(logits))
+        score = softmaxed[0, 1]
 
-    def format_prompt(self, own: str, oppo: str) -> str:
+        if score > 0.15:
+            predicted_class = "not to trust"
+        else:
+            predicted_class = "to trust"
+
+        return predicted_class
+
+    # def get_cicero_order_recommendations(self, own: str) -> List[str]:
+    #     """Retrieve orders from CICERO."""
+    #     sender_player = Player(self.agent, own)
+    #     sender_orders = sender_player.get_orders(self.game)
+    #     return sender_orders  # type: ignore[no-any-return]
+
+    def format_prompt(self, own: str, oppo: str, suggest_orders: List[str]) -> str:
         """Create prompt used as input to LLM."""
         if own in POWER_NAMES_DICT:
             own = POWER_NAMES_DICT[own]
@@ -111,7 +144,7 @@ class LlmAdvisor(BaselineBot):
         system_prompt = (
             "<<SYS>>\n"
             "You are an AI assistant tasked with understanding and analyzing the board status "
-            "of a Diplomacy game, the message history between two players."
+            "of a Diplomacy game, the message history between two players and"
             "the recommended orders by Cicero for the current phase.\n"
             "Your goal is to help respond to the last message from another player, "
             "based on the context provided.\n"
@@ -139,39 +172,70 @@ class LlmAdvisor(BaselineBot):
             if (message.sender == own and message.recipient == oppo)
             or (message.sender == oppo and message.recipient == own)
         ]
-        sorted_messages = sorted(filtered_messages, key=lambda x: x.time_sent, reverse=True)
-        closest_8_messages = sorted_messages[:8]
-        reversed_closest_8_messages = list(reversed(closest_8_messages))
-        my_message = ""
-        i = 0
-        while i < len(reversed_closest_8_messages):
-            my_message += f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' "
-            if (
-                i == len(reversed_closest_8_messages) - 1
-                and reversed_closest_8_messages[i].sender != oppo
-            ):
-                my_message = my_message.rsplit(
-                    f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' ",
-                    1,
-                )[0]
-                reversed_closest_8_messages.pop()
-                i -= 1
-            i += 1
-
-        # cicero recommendation format
-        if USE_CICERO:
-            sender_orders = self.get_cicero_order_recommendations(own)
+        if filtered_messages == []:
+            return None,None
         else:
-            sender_orders = self.get_random_orders()
+            sorted_messages = sorted(filtered_messages, key=lambda x: x.time_sent, reverse=True)
+            closest_8_messages = sorted_messages[:8]
+            reversed_closest_8_messages = list(reversed(closest_8_messages))
+            my_message = ""
+            i = 0
+            while i < len(reversed_closest_8_messages):
+                my_message += f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' "
+                if (
+                    i == len(reversed_closest_8_messages) - 1
+                    and reversed_closest_8_messages[i].sender != oppo
+                ):
+                    my_message = my_message.rsplit(
+                        f"Message from {reversed_closest_8_messages[i].sender}:'{reversed_closest_8_messages[i].message}' ",
+                        1,
+                    )[0]
+                    reversed_closest_8_messages.pop()
+                    i -= 1
+                i += 1
 
-        prompt = (
-            f"<s>[INST] {system_prompt}\n    \n---\n\n"
-            f"Board Status: {sorted_board_states}\n\n"
-            f"Cicero Recommendation for {own}: {sender_orders}\n\n"
-            f"Message History: {my_message}\n    \n---\n\n"
-            f"Question: I({own}) decide to trust {oppo}. As the advisor of {own}, just give me one short but not general(specific to the last message) message which I can use directly to respond to {oppo}'s last message. [/INST]"
-        )
-        return prompt
+            last_message = ""
+            for msg in reversed(reversed_closest_8_messages):
+                if msg.sender == oppo:
+                    last_message = msg.message
+                    break
+
+            prompt_for_classification = f"{oppo} sends {own}:{last_message}"
+            decision = self.classification(prompt = prompt_for_classification)
+
+            # cicero recommendation format
+            # if USE_CICERO:
+            #     sender_orders = self.get_cicero_order_recommendations(own)
+            # else:
+            #     sender_orders = self.get_random_orders()
+
+            prompt = (
+                f"<s>[INST] {system_prompt}\n    \n---\n\n"
+                f"Board Status: {sorted_board_states}\n\n"
+                f"Cicero Recommendation for {own}: {suggest_orders}\n\n"
+                f"Message History: {my_message}\n    \n---\n\n"
+                f"Last Message: Message from {oppo}: {last_message}\n    \n---\n\n"
+                f"Question: I am {own} and I decide {decision} the last message from {oppo}. As my advisor, give me one short but specific message for the last message which I can use directly to respond to {oppo}'s last message. You also need to give me the reason to support my decision. [/INST]"
+            )
+            return prompt,decision
+    
+    async def read_suggestions_from_advisor(self) -> List[str]:
+        """Read suggestions from RandomProposerAdvisor.
+
+        Returns:
+            List of suggested orders.
+        """
+        received_messages = self.read_messages()
+        
+        # Filter for messages sent by the RandomProposerAdvisor with msg_type "suggested_move_full"
+        suggestions = [
+            msg.message
+            for msg in received_messages
+            if msg.sender == "omniscient_type" and msg.type == "suggested_move_full"
+        ]
+
+        # Return a flattened list of suggested orders if there are any suggestions
+        return suggestions if suggestions else []
 
 
     async def do_messaging_round(self, orders: Sequence[str]) -> List[str]:
@@ -180,12 +244,18 @@ class LlmAdvisor(BaselineBot):
         Returns:
             List of orders to carry out.
         """
+        suggested_orders = await self.read_suggestions_from_advisor()
         for other_power in get_other_powers([self.power_name], self.game):
-            prompt = self.format_prompt(self.power_name, other_power)
-            generate_text = self.generate_text(prompt)
-            index = generate_text.find('[/INST] ')
-            model_output = generate_text[index+8:]
-            await self.suggest_message(other_power, model_output)
+            prompt,decision = self.format_prompt(self.power_name, other_power,suggested_orders)
+            if prompt == None:
+                continue
+            else:
+                generate_text = self.generate_text(prompt)
+                index = generate_text.find('[/INST] ')
+                model_output = generate_text[index+8:]
+                decision_output = f"I recommend {decision} the last message."
+                final_output = decision_output + model_output
+                await self.suggest_message(other_power, final_output)
 
         return list(orders)
 
