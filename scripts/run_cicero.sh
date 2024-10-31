0fcc61b57bb123673114495fcda72182921f34a1
Alex Hedges <git@alexhedges.dev>
2024-10-31 16:57:19 -0400

    Run merged CICERO image with `run_cicero.sh`

--- scripts/run_cicero.sh
+++ scripts/run_cicero.sh
@@ -2,13 +2,14 @@
 
 set -euxo pipefail
 
-BOT_NAME=$1
-shift 1
+BOT_TAG=$1
+BOT_TYPE=$2
+shift 2
 
 CICERO_DIR=/media/volume/cicero-base-models
 
 GAME_COMMAND=(
-  python fairdiplomacy_external/mila_api.py
+  python fairdiplomacy_external/mila_api_"$BOT_TYPE".py
   --game_type 2
   "$@"
 )
@@ -20,11 +21,11 @@ LOG_FILE=logs/$NOW.txt
 time docker run \
   --rm \
   --gpus all \
-  --name cicero_"$BOT_NAME"_"$RANDOM" \
+  --name cicero_"$BOT_TAG"_"$RANDOM" \
   --volume "$CICERO_DIR"/agents:/diplomacy_cicero/conf/common/agents:ro \
   --volume "$CICERO_DIR"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2:ro \
   --volume "$CICERO_DIR"/models:/diplomacy_cicero/models:ro \
   --workdir /diplomacy_cicero \
-  ghcr.io/allan-dip/diplomacy_cicero:"$BOT_NAME" \
+  ghcr.io/allan-dip/diplomacy_cicero:"$BOT_TAG" \
   "${GAME_COMMAND[@]}" |&
   tee "$LOG_FILE"
