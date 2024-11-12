954d8ad7204bdac4c96c949a538d10991729d9d6
Alex Hedges <git@alexhedges.dev>
2024-11-11 19:12:40 -0500

    do not import bots if cannot be instantiated

--- src/chiron_utils/bots/__init__.py
+++ src/chiron_utils/bots/__init__.py
@@ -3,21 +3,28 @@
 from typing import List, Type
 
 from chiron_utils.bots.baseline_bot import BaselineBot as BaselineBot
-from chiron_utils.bots.knn_bot import (
-    KnnAdvisor as KnnAdvisor,
-    KnnPlayer as KnnPlayer,
-)
+
 from chiron_utils.bots.random_proposer_bot import (
     RandomProposerAdvisor as RandomProposerAdvisor,
     RandomProposerPlayer as RandomProposerPlayer,
 )
 
 BOTS: List[Type[BaselineBot]] = [
-    KnnPlayer,
-    KnnAdvisor,
+
     RandomProposerAdvisor,
     RandomProposerPlayer,
 ]
+
+try:
+    from chiron_utils.bots.knn_bot import (
+        KnnAdvisor as KnnAdvisor,
+        KnnPlayer as KnnPlayer,
+    )
+    BOTS.extend([KnnPlayer,
+    KnnAdvisor,])
+except ImportError:
+    pass
+
 NAMES_TO_BOTS = {bot.__name__: bot for bot in BOTS}
 
 DEFAULT_BOT_TYPE = RandomProposerPlayer  # pylint: disable=invalid-name
