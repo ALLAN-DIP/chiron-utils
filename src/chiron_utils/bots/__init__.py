52dad7e04f21bfd0b58f3f0849be1fa799db9b14
Nicholas Hadiwijaya <nicholaswilliam2307@gmail.com>
2025-01-15 23:12:56 +1100

    add elastic message advisor bot
    update elastic message advisor
    
    update readme for running elasticbot
    
    ~
    
    fix elastic bot to be compatible with new changes
    
    update baseline models ver
    
    update baseline models version
    
    add elastic message advisor bot
    
    update elastic message advisor
    
    update readme for running elasticbot
    
    ~
    
    fix elastic bot to be compatible with new changes
    
    update baseline models version

--- src/chiron_utils/bots/__init__.py
+++ src/chiron_utils/bots/__init__.py
@@ -22,6 +22,9 @@ BOTS: List[Type[BaselineBot]] = [
 # but there currently isn't a way to check if a given extra was used during installation.
 importable_modules = set(importlib_metadata.packages_distributions())
 if {"baseline_models"} < importable_modules:
+    from chiron_utils.bots.elastic_bot import (
+        ElasticAdvisor as ElasticAdvisor,
+    )
     from chiron_utils.bots.lr_bot import (
         LrAdvisor as LrAdvisor,
         LrPlayer as LrPlayer,
@@ -45,6 +48,7 @@ if {"baseline_models"} < importable_modules:
             LrProbsTextAdvisor,
             LrProbsTextVisualAdvisor,
             LrProbsVisualAdvisor,
+            ElasticAdvisor,
         ]
     )
 if {"torch", "transformers"} < importable_modules:
