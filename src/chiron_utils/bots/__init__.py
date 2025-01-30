17859709d009449428b60a8763bb56028e950a01
Nicholas Hadiwijaya <nicholaswilliam2307@gmail.com>
2025-01-30 18:23:39 -0500

    Implement baseline `LrBot` agent
    Co-authored-by: Alex Hedges <git@alexhedges.dev>
    Co-authored-by: Alexander Jephtha <ajep3697@uni.sydney.edu.au>

--- src/chiron_utils/bots/__init__.py
+++ src/chiron_utils/bots/__init__.py
@@ -3,12 +3,18 @@
 from typing import List, Type
 
 from chiron_utils.bots.baseline_bot import BaselineBot as BaselineBot
+from chiron_utils.bots.lr_bot import (
+    LrAdvisor as LrAdvisor,
+    LrPlayer as LrPlayer,
+)
 from chiron_utils.bots.random_proposer_bot import (
     RandomProposerAdvisor as RandomProposerAdvisor,
     RandomProposerPlayer as RandomProposerPlayer,
 )
 
 BOTS: List[Type[BaselineBot]] = [
+    LrAdvisor,
+    LrPlayer,
     RandomProposerAdvisor,
     RandomProposerPlayer,
 ]
