cb1ba8952d2951469afc5bc88818ec236ddca03d
Alex Hedges <git@alexhedges.dev>
2025-06-12 22:32:20 -0400

    Consolidate opponent order suggestion parsing code
    The logic was used 5 separate times throughout the repository!

--- src/chiron_utils/bots/llm_advisor_new_bot.py
+++ src/chiron_utils/bots/llm_advisor_new_bot.py
@@ -2,7 +2,6 @@
 
 import asyncio
 from dataclasses import dataclass, field
-import json
 import random
 from typing import Dict, List, Optional, Sequence, Tuple, Union
 
@@ -126,7 +125,7 @@ A MUN - BUR:
         return "\n".join(lines)
 
     def format_prompt_phase1(
-        self, own: str, suggest_orders: List[str], own_orders: List[str]
+        self, own: str, suggest_orders: Dict[str, List[str]], own_orders: List[str]
     ) -> Optional[str]:
         """Create prompt used as input to the LLM.
 
@@ -143,11 +142,8 @@ A MUN - BUR:
         sorted_board_states = {key: sorted(value) for key, value in board_states.items()}
         formated_board_states = self.format_boardstates(sorted_board_states)
         # opponent order prediction
-        parsed_data = json.loads(suggest_orders[0])
-        predicted_orders = parsed_data["payload"]["predicted_orders"]
-        formatted_opponent_orders = predicted_orders
         formatted_opponent_orders = "\n".join(
-            f"{power}: " + ", ".join(predicted_orders[power]) for power in predicted_orders
+            f"{power}: " + ", ".join(suggest_orders[power]) for power in suggest_orders
         )
         # own recommended order format
         formatted_recommended_orders = ", ".join(own_orders)
@@ -172,7 +168,11 @@ A MUN - BUR:
         return prompt
 
     def format_prompt_phase2(
-        self, own: str, suggest_orders: List[str], own_orders: List[str], rationales: Optional[str]
+        self,
+        own: str,
+        suggest_orders: Dict[str, List[str]],
+        own_orders: List[str],
+        rationales: Optional[str],
     ) -> Optional[str]:
         """Create prompt used as input to the LLM.
 
@@ -186,12 +186,8 @@ A MUN - BUR:
         sorted_board_states = {key: sorted(value) for key, value in board_states.items()}
         formated_board_states = self.format_boardstates(sorted_board_states)
 
-        parsed_data = json.loads(suggest_orders[0])
-        predicted_orders = parsed_data["payload"]["predicted_orders"]
-        formatted_opponent_orders = predicted_orders
-
         formatted_opponent_orders = "\n".join(
-            f"{power}: " + ", ".join(predicted_orders[power]) for power in predicted_orders
+            f"{power}: " + ", ".join(suggest_orders[power]) for power in suggest_orders
         )
         formatted_recommended_orders = ", ".join(own_orders)
         if formatted_recommended_orders == "":
