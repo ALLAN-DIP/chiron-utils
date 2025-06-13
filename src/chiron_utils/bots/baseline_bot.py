6d3e6cae2db33c7413eab1935dc93e205740e547
Alex Hedges <git@alexhedges.dev>
2025-06-12 22:22:34 -0400

    Move order suggestion reading methods to `BaselineBot`
    These are general-purpose methods that are useful to have for any bot.

--- src/chiron_utils/bots/baseline_bot.py
+++ src/chiron_utils/bots/baseline_bot.py
@@ -218,6 +218,21 @@ class BaselineBot(ABC):
             suggestion_type=suggestion_type,
         )
 
+    def read_own_suggestions_from_advisor(self) -> List[str]:
+        """Read recommended orders from advisor.
+
+        Returns:
+            List of recommended orders.
+        """
+        received_messages = self.read_messages()
+        suggestion_messages = [
+            msg.message
+            for msg in received_messages
+            if msg.type == diplomacy_strings.SUGGESTED_MOVE_FULL
+        ]
+        logger.info("%s received own move suggestions: %s", self.display_name, suggestion_messages)
+        return suggestion_messages
+
     async def suggest_opponent_orders(self, opponent_orders: Mapping[str, Sequence[str]]) -> None:
         """Send predicted orders for opponent powers to the server.
 
@@ -247,6 +262,25 @@ class BaselineBot(ABC):
             payload=payload,
             suggestion_type=diplomacy_strings.SUGGESTED_MOVE_OPPONENTS,
         )
+
+    def read_opponent_move_suggestions_from_advisor(self) -> List[str]:
+        """Read opponent move predictions from advisor.
+
+        Returns:
+            List of predicted orders.
+        """
+        received_messages = self.read_messages()
+        suggestion_messages = [
+            msg.message
+            for msg in received_messages
+            if msg.type == diplomacy_strings.SUGGESTED_MOVE_OPPONENTS
+        ]
+        logger.info(
+            "%s received opponent move suggestions: %s", self.display_name, suggestion_messages
+        )
+        return suggestion_messages
+
+    read_suggestions_from_advisor = read_opponent_move_suggestions_from_advisor
 
     async def suggest_orders_probabilities(
         self, province: str, orders_probabilities: Mapping[str, OrderProbability]
