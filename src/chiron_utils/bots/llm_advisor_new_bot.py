c3ce644f0da5f0ab1467bc1ac7afaff41961079f
Alex Hedges <git@alexhedges.dev>
2025-06-12 22:23:19 -0400

    Rename order suggestion reading methods in `BaselineBot`

--- src/chiron_utils/bots/llm_advisor_new_bot.py
+++ src/chiron_utils/bots/llm_advisor_new_bot.py
@@ -323,10 +323,10 @@ A MUN - BUR:
         if own in POWER_NAMES_DICT:
             own = POWER_NAMES_DICT[own]
 
-        filtered_opponent_orders = self.read_opponent_move_suggestions_from_advisor()
+        filtered_opponent_orders = self.read_suggested_opponent_orders()
         if not filtered_opponent_orders:
             return []
-        filtered_own_orders = self.read_own_suggestions_from_advisor()
+        filtered_own_orders = self.read_suggested_orders()
         if not filtered_own_orders:
             return []
         other_power = get_other_powers([self.power_name], self.game)
