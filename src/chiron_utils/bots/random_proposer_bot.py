3564017c4dfe51d2c70b72550e9e47c2792b8432
Alex Hedges <git@alexhedges.dev>
2025-07-14 15:33:49 -0400

    [TEST] Do not send message or commentary advice from `RandomProposerAdvisor`

--- src/chiron_utils/bots/random_proposer_bot.py
+++ src/chiron_utils/bots/random_proposer_bot.py
@@ -72,8 +72,7 @@ class RandomProposerBot(BaselineBot, ABC):
 
         for other_power, suggested_random_orders in random_order_proposals.items():
             if self.bot_type == BotType.ADVISOR:
-                await self.suggest_message(other_power, suggested_random_orders)
-                await self.suggest_commentary(other_power, f"I have advice about {other_power}!")
+                pass
             elif self.bot_type == BotType.PLAYER:
                 await self.send_message(other_power, suggested_random_orders)
 
