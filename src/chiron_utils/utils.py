7f8402231428b1743da7911d7c2f3d21d29232bc
Alex Hedges <git@alexhedges.dev>
2025-07-28 14:42:05 -0400

    [TEMP] Remove monkey patching in logging code
    It is safer to modify an instance variable than a class variable.

--- src/chiron_utils/utils.py
+++ src/chiron_utils/utils.py
@@ -22,14 +22,13 @@ def return_logger(name: str, log_level: int = logging.INFO) -> logging.Logger:
     Returns:
         An initialized logger.
     """
-    # Monkey patch module to show milliseconds
-    logging.Formatter.default_msec_format = "%s.%03d"
-
     logging.basicConfig(
         format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",
         # Fall back to `WARNING`, the default level of the root logger, if `log_level` is `NOTSET`
         level=log_level or logging.WARNING,
     )
+    logging.root.handlers[0].formatter.default_msec_format = "%s.%03d"  # type: ignore[union-attr]
+
     new_logger = logging.getLogger(name)
     new_logger.setLevel(log_level)
     return new_logger
